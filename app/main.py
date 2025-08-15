f1ed6146f24c11ac515d2754830ccbb292ec60f9
GurveerSinghDev <gurveersingh0311@gmail.com>
2025-08-15 18:40:31 +0100

    Api Fix
    Api fully works now

--- app/main.py
+++ app/main.py
@@ -6,15 +6,23 @@ app = FastAPI()
 
 @app.post("/uploadpdf/")
 async def upload_pdf(file: UploadFile = File(...)):
-    temp_path = f"temp/{file.filename}" # creates a temporary filename
-    with open(temp_path, "wb") as f:  
-        shutil.copyfileobj(file.file, f)   # copies contents to a new temp file
+    upload_dir = "temp"
+    os.makedirs(upload_dir, exist_ok=True)  #ensure the temp directory exists
+
+    temp_path = os.path.join(upload_dir, file.filename)  #creates a temporary filename
 
     try:
+        with open(temp_path, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+
         text = parsePdf(temp_path)
-    except Exception as e:
+
+    except Exception as e:            #error handling
         return {"error": str(e)}
+
     finally:
-        os.remove(temp_path) # removes the temp file after processing
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
 
     return {"text": text}
+   
